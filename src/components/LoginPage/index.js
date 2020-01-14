dc7fc1435d119f1fb901b40c8ba8532d8d4d086c
Gilson Viana <vianawebdev@gmail.com>
2020-01-13 23:27:41 -0500

    :construction: Login page

---
+++ src/components/LoginPage/index.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Toast from 'react-bootstrap/Toast'
+
+import logo from '../../assets/images/logo.png'
+
+import "./style.css";
+
+const LoginPage = props => {
+  const [showToast, setShowToast] = React.useState(true)
+
+  return (
+    <div className="page__login">
+      <Toast onClose={() => setShowToast(false)} show={showToast} delay={5000} autohide style={{
+        position: 'absolute',
+        left: `50%`,
+        transform: `translateX(${-50}%)`,
+        backgroundColor: `#eb5a46`,
+        color: `#eee`
+      }}>
+          <Toast.Body>Unable to login user. Email or password is wrong.</Toast.Body>
+        </Toast>
+      <Container fluid style={{
+        justifyContent: 'space-between'
+      }}>
+        <header className="page__login__header">
+          <img src={logo} />
+        </header>
+        <Row>
+          <Col xs={12} md={{ span: 4, offset: 4 }}>
+            <section className="page__login__content">
+              <div className="page__login__content__box">
+                <div className="page__login__content__box__form">
+                  <h3 className="page__login__content__box__form__title">Log in to Athletes Pro</h3>
+                  <form>
+                    <input type="text" placeholder="Enter email" />
+                    <input type="password" placeholder="Enter password" />
+                    <button type="submit">Log in</button>
+                  </form>
+                  <div className="page__login__content__footer">
+                    <Link to="">Forgot password?</Link>
+                    <Link to="">Sign up for an account</Link>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </Col>
+        </Row>
+        <Row style={{
+          position: 'absolute',
+          width: `100%`,
+          bottom: 0,
+          left: 0,
+          height: `3rem`
+        }}>
+          <Col xs={12} md={{ span: 4, offset: 4 }}>
+            <footer className="page__login__footer">
+              <Link to="">Privacy policy</Link>
+              <p>/</p>
+              <Link to="">Terms of service</Link>
+            </footer>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default LoginPage;
