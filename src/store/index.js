8ff03aee70fcff993817843f9b015c64f721a004
Gilson Viana <vianawebdev@gmail.com>
2020-01-21 15:07:41 -0500

    :construction: Redux persist configured

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import { composeWithDevTools } from 'redux-devtools-extension';
 import subscriptionPlans from './subscriptionPlans/reducer'
 import auth from './auth/reducer'
@@ -11,11 +13,20 @@ const reducers = combineReducers({
     error
 })
 
-export const configureStore = () => {
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+   
+const persistedReducer = persistReducer(persistConfig, reducers)
+
+export default () => {
     const middlewares = [thunk]
     const enhancedMiddlewares = applyMiddleware(...middlewares)
+    // const store = createStore(reducers, composeWithDevTools(enhancedMiddlewares))
 
-    const store = createStore(reducers, composeWithDevTools(enhancedMiddlewares))
+    let store = createStore(persistedReducer, {}, composeWithDevTools(enhancedMiddlewares))
+    let persistor = persistStore(store)
 
-    return store
+    return { store, persistor }
 }
