47f97cc36c0b524a968638cbf18575a5abd0b176
Gilson Viana <vianawebdev@gmail.com>
2020-01-30 19:15:36 -0500

    :construction: Evaluation details chart

---
+++ src/components/Dashboard/Main/EvaluationDetails/EvaluationDetailsOverview/EvaluationDetailsActivity/EvaluationDetailsChart/EvaluationDetailsChartEmptyState/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const EvaluationDetailsChartEmptyState = () => {
+    return (    
+        <div className="widget__evaluation-details-chart-empty-state bg-light p-md-3">
+            <p className="lead m-auto text-center">
+                Select a metric to see your performance.
+            </p>
+        </div>
+    )
+}
+
+export default EvaluationDetailsChartEmptyState
--- src/components/Dashboard/Main/EvaluationDetails/EvaluationDetailsOverview/EvaluationDetailsActivity/EvaluationDetailsChart/index.js
+++ src/components/Dashboard/Main/EvaluationDetails/EvaluationDetailsOverview/EvaluationDetailsActivity/EvaluationDetailsChart/index.js
@@ -1,15 +1,51 @@
-import React from 'react'
-import Card from 'react-bootstrap/Card'
+// Dependencies
+import React, { useMemo } from "react";
+// @https://github.com/tannerlinsley/react-charts
+import { Chart } from "react-charts";
+import Card from "react-bootstrap/Card";
+
+// Components
+import EvaluationDetailsChartEmptyState from './EvaluationDetailsChartEmptyState'
 
 const EvaluationDetailsChart = () => {
-    return (
-        <Card className="evaluation-details-chart shadow-sm pt-3 px-3 mb-4 bg-white rounded">
-            <Card.Title>Activity</Card.Title>
-            <Card.Body>
-                <p>EvaluationDetailsChart</p>
-            </Card.Body>
-        </Card>
-    )
-}
+  const data = useMemo(
+    () => [
+      {
+        label: "Series 1",
+        data: [
+          { x: 1, y: 10 },
+          { x: 2, y: 13 },
+          { x: 3, y: 15 }
+        ]
+      }
+    ],
+    []
+  )
+  
+  const axes = React.useMemo(
+    () => [
+      { primary: true, type: 'linear', position: 'bottom' },
+      { type: 'linear', position: 'left' }
+    ],
+    []
+  )
 
-export default EvaluationDetailsChart
+  return (
+    <Card className="evaluation-details-chart shadow-sm pt-3 px-3 mb-4 bg-white rounded">
+      <Card.Title>Activity</Card.Title>
+      <Card.Body>
+        <EvaluationDetailsChartEmptyState />
+        {/* <div
+          style={{
+            width: '100%',
+            height: '15rem'
+          }}
+        >
+          <Chart data={data} axes={axes} />
+        </div> */}
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default EvaluationDetailsChart;
