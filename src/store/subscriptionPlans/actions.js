785231039d3567855ae3ed551da059e59829e48b
Gilson Viana <vianawebdev@gmail.com>
2020-05-02 11:26:14 -0300

    :sparkles: Update PayPal subscription plans

--- src/store/subscriptionPlans/actions.js
+++ src/store/subscriptionPlans/actions.js
@@ -1,11 +1,14 @@
-import { SET_SUBSCRIPTION_PLANS } from './types'
+import { SET_SUBSCRIPTION_PLANS } from "./types";
 
 export const getSubscriptionPlans = () => {
-    //TODO make http call to API
-    return dispatch => {
-        dispatch({
-            type: SET_SUBSCRIPTION_PLANS,
-            payload: [{ plan_id: 'P-9W02681611507760NL2PCOHA', name: 'Monthly' }, { plan_id: 'P-3NX85101N61441605L2PCOZI', name: 'Yearly' }]
-        })
-    }
-}
+  //TODO make http call to API
+  return (dispatch) => {
+    dispatch({
+      type: SET_SUBSCRIPTION_PLANS,
+      payload: [
+        { plan_id: "P-5RH556461F121723WL2WYCUA", name: "Monthly" },
+        { plan_id: "P-87659893M77501912L2WYAZY", name: "Yearly" },
+      ],
+    });
+  };
+};
